b83c75a557b98a1d89c1ef05f512f2cfbb151991
tavisca-rgupta-2019 <rgupta@tavisca.in>
2019-07-05 22:21:13 +0530

    Tdd basics 2 assignment

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,87 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator obj = new Calculator();
+        string result = null;
+       
+       
         [Fact]
-        public void DummyTest()
+        public void TestCase1()
         {
-            return;
+            string testcase = "10+2=";
+            
+            foreach (char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Assert.Equal("12", result);
+            
+
         }
+        [Fact]
+        public void Testcase2()
+        {
+            string testcase = "00..001";
+            foreach(char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Assert.Equal("0.001", result);
+            
+            
+        }
+        [Fact]
+
+        public void Testcase3()
+        {
+            string testcase = "10/0=";
+            foreach (char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Assert.Equal("-E-", result);
+        }
+
+
+        
+            [Fact]
+        public void Testcase4()
+        {
+            string testcase = "12+2SSS=";
+            foreach (char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Assert.Equal("10", result);
+
+
+        }
+        [Fact]
+        public void Testcase5()
+        {
+            string testcase = "1+2+C";
+            foreach (char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Assert.Equal("0", result);
+
+
+        }
+        [Fact]
+        public void Testcase6()
+        {
+            string testcase = "1+2+3+=";
+            foreach (char c in testcase)
+            {
+                result = obj.SendKeyPress(c);
+            }
+            Console.WriteLine(result);
+            Assert.Equal("6", result);
+
+
+        }
+
+
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -2,13 +2,156 @@
 
 namespace ConsoleCalculator
 {
+    public class Decalrations
+    {
+        public const string operands = "0123456789.";
+        public const string operators = "+-/xX";
+        public const string reset = "Cc";
+        public const string toggle = "sS";
+        public const string equalto = "=";
+        public string eval(string s1,string s2,char? op)
+        {
+            string result = null;
+            
+            
+                float num1 = float.Parse(s1);
+                float num2 = float.Parse(s2);
+                float res;
+                switch (op)
+                {
+                    case '+':
+                        res = num1 + num2;
+                        result = res.ToString();
+                        break;
+                    case '-':
+                        res = num1 - num2;
+                        result = res.ToString();
+                        break;
+                    case '/':
+                        if(num2==0)
+                        {
+                            result = "-E-";
+                            break;
+                        }
+                        res = num1 / num2;
+                        result = res.ToString();
+                        break;
+                    case 'x':
+                    case 'X':
+                        res = num1 * num2;
+                        result = res.ToString();
+                        break;
+                }
+            
+            
+            return result;
+
+
+        }
+
+    }
     public class Calculator
     {
+        string s1 = null;
+        string s2 = null;
+        char? ope=null;
+        string result = null;
+        Decalrations obj = new Decalrations();
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
-        }
+            if (Decalrations.operands.Contains(key.ToString()))
+            {
+                if (ope == null)
+                {
+                    if (s1 == "0" && key == '0')
+                        result = s1;
+                    else if(key=='.' && s1.Contains("."))
+                    {
+                        result = s1;
+                    }
+                    else
+                    {
+                        s1 = s1 + key;
+                        result = s1;
+                    }
+                }
+                else
+                {
+                    if (s2 == "0" && key == '0')
+                        result = s2;
+                    else if (key == '.' && s2.Contains("."))
+                        result = s2;
+                    else
+                    {
+                        s2 = s2 + key;
+                        result = s2;
+                    }
+
+
+                }
+
+
+            }
+            else if (Decalrations.operators.Contains(key.ToString()))
+            {
+                if (ope != null)
+                {
+                    s1 = obj.eval(s1, s2, ope);
+                    s2 = null;
+                }
+
+                
+                    ope = key;
+                    result = s1;
+                
+            }
+            else if (Decalrations.reset.Contains(key.ToString()))
+            {
+                s1 = null;
+                s2 = null;
+                result = "0";
+            }
+            else if (Decalrations.toggle.Contains(key.ToString()))
+            {
+                if (ope == null)
+                {
+                    if (s1[0] == '-')
+                    {
+                        s1 = s1.Substring(1);
+                    }
+                    else
+                    {
+                        s1 = "-" + s1;
+                    }
+                }
+                else
+                {
+                    if (s2[0] == '-')
+                    {
+                        s2 = s2.Substring(1);
+                    }
+                    else
+                    {
+                        s2 = "-" + s2;
+                    }
+                }
+            }
+            else
+            {   if(s2==null)
+                {
+                    result = s1;
+                }
+            else
+                result = obj.eval(s1, s2, ope);
+            }
+                
+            return result;
+
+               
+
+            }
+        
     }
 }
+
